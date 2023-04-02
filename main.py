b099e4f726a8a2e42274ea004477fd655dba1709
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-02 18:52:21 -0300

    added code

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+#Receiving value from user
+int1 = int(input("Digite um número inteiro : \n"))
+int2 = int(input("Digite outro número inteiro : \n"))
+real = float(input("Digite um número real : \n"))
 
+#Calculating instructions
+#A
+operation = (int1 * 2) * (int2 / 2)
+
+#B
+operation2 = (int1 * 3) + real
+
+#C
+operation3 = real **3
+
+#Printing results for the user
+print(f"No item A será calculado o dobro do 1° número vezes a metade do 2°. \nNo item B será calculado o triplo do 1° mais o 3°. \nNo item C será calculado o cubo do 3°.")
+print(f"A. {operation}")
+print(f"B. {operation2}")
+print(f"C. {operation3}")
